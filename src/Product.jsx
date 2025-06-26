ffe7f1e7a9b82e206220b804106a98e6e66f39b8
Rishika <rishika@example.com>
2025-06-26 13:49:30 +0530

    first commit

--- src/Product.jsx
+++ src/Product.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import main from './assets/main.svg';
 import arrow from './assets/arrow.svg';
 import small from './assets/small.svg';
-import icon from './assets/icon.svg';
+import icon from './assets/Icon.svg';
 import share from './assets/share.svg';
 import heart from './assets/heart.svg';
 import Rating from './Rating';
